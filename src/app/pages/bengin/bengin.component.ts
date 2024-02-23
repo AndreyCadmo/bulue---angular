93a6abf9adf1e981f71e9cdcc4d3eae4d4481b57
Andrey Cadmo <andreycadmo06@gmail.com>
2024-02-23 19:06:49 -0300

    criando a pagina bengin engine

---
+++ src/app/pages/bengin/bengin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bengin',
+  standalone: true,
+  imports: [],
+  templateUrl: './bengin.component.html',
+  styleUrl: './bengin.component.css'
+})
+export class BenginComponent {
+
+}

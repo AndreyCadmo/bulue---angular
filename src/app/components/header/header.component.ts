6ebb83e7369049d3de410ab10b253d66493f73de
Andrey Cadmo <andreycadmo06@gmail.com>
2024-02-23 19:03:20 -0300

    criando o componente header

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: "app-header",
+  standalone: true,
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
+})
+export class HeaderComponent {
+
+  public menuButton(): void {
+    console.log('Chamou func')
+    const divMenu = document.getElementById("menu");
+    divMenu?.classList.toggle('hidden');
+  }
+}

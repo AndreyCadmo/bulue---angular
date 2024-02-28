c86f32a6561f29e215c42d8ec80909896723078b
Andrey Cadmo <andreycadmo06@gmail.com>
2024-02-28 15:23:59 -0300

    criando uma rota para o 404

--- src/app/configs/app.routes.ts
+++ src/app/configs/app.routes.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from '../pages/home/home.component';
 import { SupportComponent } from '../pages/support/support.component';
 import { BenginComponent } from '../pages/bengin/bengin.component';
 import { ConstructionComponent } from '../pages/construction/construction.component';
+import { ErrorComponent } from '../pages/error/error.component';
 
 export const routes: Routes = [
   { path: "news", component: NewsComponent },
   { path: "support", component: SupportComponent },
   { path: "bengin", component: BenginComponent },
   { path: "construction", component: ConstructionComponent },
-  { path: "**", component: HomeComponent},
-  { path: "", component: HomeComponent},
+  { path: "home", component: HomeComponent},
+  { path: "", redirectTo: "home", pathMatch: 'full'},
+  { path: "**", component: ErrorComponent},
 ];

28077e81f48e1301dd00bf6b4c2d9d04c87146cf
Andrey Cadmo <andreycadmo06@gmail.com>
2024-02-29 13:21:51 -0300

    criando uma rota para a pagina creator

--- src/app/configs/app.routes.ts
+++ src/app/configs/app.routes.ts
@@ -5,12 +5,14 @@ import { SupportComponent } from '../pages/support/support.component';
 import { BenginComponent } from '../pages/bengin/bengin.component';
 import { ConstructionComponent } from '../pages/construction/construction.component';
 import { ErrorComponent } from '../pages/error/error.component';
+import { CreatorComponent } from '../pages/creator/creator.component';
 
 export const routes: Routes = [
   { path: "news", component: NewsComponent },
   { path: "support", component: SupportComponent },
   { path: "bengin", component: BenginComponent },
   { path: "construction", component: ConstructionComponent },
+  { path: "creator", component: CreatorComponent},
   { path: "home", component: HomeComponent},
   { path: "", redirectTo: "home", pathMatch: 'full'},
   { path: "**", component: ErrorComponent},
